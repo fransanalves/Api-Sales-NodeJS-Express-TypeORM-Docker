8c349ee03128f9a42f93db211b812ebf89f10e43
Francisca <fransanalves@outlook.com>
2022-09-09 15:01:59 -0300

    Criação do arquivo server.

---
+++ src/shared/http/server.ts
@@ -0,0 +1,36 @@
+import 'reflect-metadata';
+import express, { NextFunction, Request, Response } from 'express';
+import 'express-async-errors';
+import cors from 'cors';
+import { routes } from './routes';
+import { MessageError } from '@shared/errors/MessageError';
+import '@shared/typeorm/data-source';
+import { DataSourceConnection } from '@shared/typeorm/data-source';
+
+DataSourceConnection.initialize().then(() => {
+  const server = express();
+
+  server.use(cors());
+  server.use(express.json());
+
+  server.use(routes);
+
+  server.use(
+    (error: Error, req: Request, res: Response, next: NextFunction) => {
+      if (error instanceof MessageError) {
+        return res.status(error.statusCode).json({
+          status: 'error',
+          message: error.message,
+        });
+      }
+      return res.status(500).json({
+        status: 'error',
+        message: 'Internal server error',
+      });
+    },
+  );
+
+  server.listen(3333, () => {
+    console.log('Server started on port 3333.');
+  });
+});
