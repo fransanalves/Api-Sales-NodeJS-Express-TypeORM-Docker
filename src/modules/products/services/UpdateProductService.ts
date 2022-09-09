7cee3219c469924c1bf0892ba3ea4b4162696829
Francisca <fransanalves@outlook.com>
2022-09-09 14:54:34 -0300

    Criação da classe de serviço UpdateProduct.

---
+++ src/modules/products/services/UpdateProductService.ts
@@ -0,0 +1,36 @@
+import MessageError from '@shared/errors/MessageError';
+import { Product } from '../typeorm/entities/Product';
+import { ProductsRepository } from '../typeorm/repositories/ProductsRepository';
+
+interface IProduct {
+  id: string;
+  name: string;
+  price: number;
+  quantity: number;
+}
+
+export class UpdateProductService {
+  public async execute({
+    id,
+    name,
+    price,
+    quantity,
+  }: IProduct): Promise<Product> {
+    const productRepository = ProductsRepository;
+    const product = await productRepository.findOne({ where: { id } });
+    const productNameExists = await productRepository.findByName(name);
+    if (!product) {
+      throw new MessageError('Product not found.');
+    }
+    if (productNameExists && product.name !== name) {
+      throw new MessageError('There is already one product with this name');
+    }
+    product.name = name;
+    product.price = price;
+    product.quantity = quantity;
+
+    await productRepository.save(product);
+
+    return product;
+  }
+}
