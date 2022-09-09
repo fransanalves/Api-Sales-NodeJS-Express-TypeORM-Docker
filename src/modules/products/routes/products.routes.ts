5eb00a8dff2bdd9237335af32a1d2f987713af32
Francisca <fransanalves@outlook.com>
2022-09-09 16:22:16 -0300

    Validação de rotas com celebrate.

--- src/modules/products/routes/products.routes.ts
+++ src/modules/products/routes/products.routes.ts
@@ -1,11 +1,51 @@
 import { Router } from 'express';
 import { ProductsController } from '../controllers/ProductsController';
+import { celebrate, Joi, Segments } from 'celebrate';
 
 export const router = Router();
 const controller = new ProductsController();
 
 router.get('/', controller.list);
-router.get('/:id', controller.show);
-router.post('/', controller.create);
-router.put('/:id', controller.update);
-router.delete('/:id', controller.delete);
+router.get(
+  '/:id',
+  celebrate({
+    [Segments.PARAMS]: {
+      id: Joi.string().uuid().required(),
+    },
+  }),
+  controller.show,
+);
+router.post(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      price: Joi.number().precision(2).required(),
+      quantity: Joi.number().required(),
+    },
+  }),
+  controller.create,
+);
+router.put(
+  '/:id',
+  celebrate({
+    [Segments.PARAMS]: {
+      id: Joi.string().uuid().required(),
+    },
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      price: Joi.number().precision(2).required(),
+      quantity: Joi.number().required(),
+    },
+  }),
+  controller.update,
+);
+router.delete(
+  '/:id',
+  celebrate({
+    [Segments.PARAMS]: {
+      id: Joi.string().uuid().required(),
+    },
+  }),
+  controller.delete,
+);
