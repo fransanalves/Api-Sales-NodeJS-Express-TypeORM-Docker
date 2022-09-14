26d03dc360747b6fd397d3e1580dad2bba96f8e2
Francisca <fransanalves@outlook.com>
2022-09-14 18:46:00 -0300

    Criação da rota de sessões do usuário.

---
+++ src/modules/users/routes/sessions.routes.ts
@@ -0,0 +1,17 @@
+import { celebrate, Joi, Segments } from 'celebrate';
+import { Router } from 'express';
+import { SessionsController } from '../controllers/SessionsController';
+
+export const sessionsRouter = Router();
+const sessionsController = new SessionsController();
+
+sessionsRouter.post(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      email: Joi.string().email().required(),
+      password: Joi.string().required(),
+    },
+  }),
+  sessionsController.create,
+);
