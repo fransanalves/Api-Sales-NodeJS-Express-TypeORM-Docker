8b972176d38b5d02508eda5e41ed930ad09914fa
Francisca <fransanalves@outlook.com>
2022-09-23 12:32:44 -0300

    Criação da rota de password.

---
+++ src/modules/users/routes/password.routes.ts
@@ -0,0 +1,16 @@
+import { celebrate, Joi, Segments } from 'celebrate';
+import { Router } from 'express';
+import { ForgotPasswordController } from '../controllers/ForgotPasswordController';
+
+export const passwordRouter = Router();
+const forgotPasswordController = new ForgotPasswordController();
+
+passwordRouter.post(
+  '/forgot',
+  celebrate({
+    [Segments.BODY]: {
+      email: Joi.string().email().required(),
+    },
+  }),
+  forgotPasswordController.create,
+);
