0bf54a7f8680ce37b13ddd9f19158614f7cabbdc
Francisca <fransanalves@outlook.com>
2022-09-26 16:51:51 -0300

    Criação da classe ResetPasswordController.

---
+++ src/modules/users/controllers/ResetPasswordController.ts
@@ -0,0 +1,12 @@
+import { Request, Response } from 'express';
+import { ResetPasswordService } from '../services/ResetPasswordService';
+
+export class ResetPasswordController {
+  public async create(req: Request, res: Response): Promise<Response> {
+    const { token, password } = req.body;
+    const resetPassword = new ResetPasswordService();
+    await resetPassword.execute({ token, password });
+
+    return res.status(204).json();
+  }
+}
