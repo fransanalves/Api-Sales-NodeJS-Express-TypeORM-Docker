7cb065cc7606deda02e08c7902f4696776365a5b
Francisca <fransanalves@outlook.com>
2022-09-29 15:01:11 -0300

    Alteração dos parâmetros no método sendMail.

--- src/modules/users/services/SendForgotPasswordEmailService.ts
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -1,6 +1,7 @@
 import { MessageError } from '@shared/errors/MessageError';
 import { UsersRepository } from '../typeorm/repositories/UsersRepository';
 import { UserTokensRepository } from '../typeorm/repositories/UserTokensRepository';
+import { EtherealMail } from '@config/mail/EtherealMail';
 
 interface IRequest {
   email: string;
@@ -11,12 +12,23 @@ export class SendForgotPasswordEmailService {
     const usersRepository = UsersRepository;
     const userTokensRepository = UserTokensRepository;
     const user = await usersRepository.findByEmail(email);
-
     if (!user) {
       throw new MessageError('User does not exists.');
     }
-    console.log('USER: ', user);
-    const token = await userTokensRepository.generate(user.id);
-    console.log('TOKEN: ', token);
+    const { token } = await userTokensRepository.generate(user.id);
+    await EtherealMail.sendEmail({
+      to: {
+        name: user.name,
+        email: user.email,
+      },
+      subject: '[API Vendas] Recuperação de Senha',
+      templateData: {
+        template: `Olá {{name}}: {{token}}`,
+        variables: {
+          name: user.name,
+          token,
+        },
+      },
+    });
   }
 }
