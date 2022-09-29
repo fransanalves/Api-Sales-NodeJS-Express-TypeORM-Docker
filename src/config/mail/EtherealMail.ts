e1de7225ae668919047027e312a7b6eac6a4e05a
Francisca <fransanalves@outlook.com>
2022-09-29 14:54:03 -0300

    Alterações no método sendMail.

---
+++ src/config/mail/EtherealMail.ts
@@ -0,0 +1,38 @@
+import { ISendMail } from '@config/interfaces/sendMail';
+import nodemailer from 'nodemailer';
+import { HandlebarsMailTemplate } from './HandlebarsMailTemplate';
+
+export class EtherealMail {
+  static async sendEmail({
+    to,
+    from,
+    subject,
+    templateData,
+  }: ISendMail): Promise<void> {
+    const account = await nodemailer.createTestAccount();
+    const mailTemplate = new HandlebarsMailTemplate();
+    const transporter = nodemailer.createTransport({
+      host: account.smtp.host,
+      port: account.smtp.port,
+      secure: account.smtp.secure,
+      auth: {
+        user: account.user,
+        pass: account.pass,
+      },
+    });
+    const message = await transporter.sendMail({
+      from: {
+        name: from?.name || 'Equipe API Vendas',
+        address: from?.email || 'equipe@apivendas.com.br',
+      },
+      to: {
+        name: to.name,
+        address: to.email,
+      },
+      subject,
+      html: await mailTemplate.parse(templateData),
+    });
+    console.log('Message sent: %s', message.messageId);
+    console.log('Preview URL: %s', nodemailer.getTestMessageUrl(message));
+  }
+}
